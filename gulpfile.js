eb572750954dd15b302a1d34bc951c447335f98a
Ryan Parag <rparag@rparag-imac.masonite.com>
2019-09-05 08:30:27 -0400

    common template tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
 const gulp = require('gulp')
 const mjml = require('gulp-mjml')
 const imagemin = require('gulp-imagemin')
+const del = require('del')
 
 const path = {
-  mjml: 'src/*.mjml',
+  mjml: 'src/**/*.mjml',
   public: './public',
   images: 'src/images/*.png',
   imageDest: './public/images'
 };
 
 // These are a list of our supported companies. They should match the folder names in the src/includes
-const companies = {
+const companies = [
   'advisar',
   'usawd',
   'masonite'
-}
+]
 
 // Task for compiling common templates and moving to the public folder under a folder corresponding to the respective company. Common templates are email templates that every compnany in the list will use.
 // e.g.
@@ -22,9 +23,33 @@ const companies = {
 //         | /advisar
 //                 | template.html
 
-gulp.task('commonCompile', function () {
-
+// This task copies all of the common templates into the company-specific includes folder
+gulp.task('copyFiles', async function() {
+  companies.forEach(company => {
+    return gulp
+      .src('src/common/*.mjml')
+      .pipe(gulp.dest(`src/includes/${company}/common`))
+  })
 })
+
+// This task deletes the copied common templates in the company-specific includes folder., subsequent to compiling
+gulp.task('clean', async function(company) {
+  companies.forEach(company => {
+    return del(['src/includes/' + company + '/common/**', '!src/includes/' + company + '/common'])
+  })
+})
+
+// Compile all common templates that were copied into the includes folder
+gulp.task('commonCompile', async function () {
+  companies.forEach(company => {
+    return gulp.src(`src/includes/${company}/common/**/*.mjml`)
+      .pipe(mjml())
+      .pipe(gulp.dest('public/' + company + '/'))
+  })
+})
+
+// Glob task for copying, compiling, and deleting common templates
+gulp.task('asdf', gulp.series('copyFiles','commonCompile','clean'))
 
 // Task for compiling specific templates and moving to the public folder under a folder corresponding to the respective company. Specific templates are one-off templates that are proprietary to a specific company.
 // e.g.
@@ -32,16 +57,19 @@ gulp.task('commonCompile', function () {
 //         | /advisar
 //                 | template.html
 
-gulp.task('specificCompile', function () {
+/*gulp.task('specificCompile', function () {
 
-})
+})*/
 
 // Task for optimizing images
-gulp.task('images', function () {
+/*gulp.task('images', function () {
   return gulp.src(path.images)
     .pipe(imagemin())
     .pipe(gulp.dest(path.imageDest))
 })
+
+// Bulk compile task
+gulp.task('compile', gulp.series('commonCompile', 'specficCompile', 'images'));
 
 // Watching and live reload
 gulp.task('watch', function () {
@@ -49,7 +77,7 @@ gulp.task('watch', function () {
 })
 
 // Glob Task
-gulp.task('default', gulp.series('commonCompile', 'specificCompile', 'images', 'watch'));
+gulp.task('default', gulp.series('commonCompile', 'specificCompile', 'images', 'watch'));*/
 
 /*
 gulp.task('compile', function () {
